b8d40cc0c9cf486857d9220cdc65d554d98b24ff
kuankuanli <111273868+kuankuanli@users.noreply.github.com>
2026-01-07 17:58:00 +0800

    修正 bug

--- wwwroot/js/base.js
+++ wwwroot/js/base.js
@@ -1,12 +1,9 @@
 const navbar = document.getElementById('navbar');
 
+//滑鼠滾動 __px 後， navbar 會變成實色背景
 if (navbar) {
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) { // 捲動超過 50px
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
+    navbar.classList.toggle('scrolled', window.scrollY > 56);
   });
 }
 
@@ -19,12 +16,12 @@ document.addEventListener('DOMContentLoaded', () => {
   if (nav && brand && navRightTool) {
     nav.addEventListener('show.bs.collapse', () => {
       brand.style.display = 'none';
-      navRightTool.classList.add('is-open');
+      navbar.classList.add('menu-open');
     });
 
     nav.addEventListener('hidden.bs.collapse', () => {
       brand.style.display = 'block';
-      navRightTool.classList.remove('is-open');
+      navbar.classList.remove('menu-open');
     });
   }
 
@@ -41,5 +38,36 @@ document.addEventListener('DOMContentLoaded', () => {
   // 初始化 AOS（即使沒有 navbar 也能安全執行）
   if (typeof AOS !== 'undefined') {
     AOS.init();
+  }
+});
+
+//圖片放大
+  const modal = document.getElementById("myModal");
+  const modalImg = document.getElementById("img01");
+  const captionText = document.getElementById("caption");
+  const closeBtn = document.querySelector(".close");
+
+  document.querySelectorAll(".zoom-img").forEach(img => {
+      img.addEventListener("click", () => {
+      modal.style.display = "flex";
+      modalImg.src = img.src;
+      captionText.textContent = img.alt;
+
+      // 🔒 鎖住背景滾動
+      document.body.classList.add("modal-open");
+      });
+  });
+
+  closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+
+      // 🔓 還原滾動
+      document.body.classList.remove("modal-open");
+  });
+
+  modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+    document.body.classList.remove("modal-open");
   }
 });
